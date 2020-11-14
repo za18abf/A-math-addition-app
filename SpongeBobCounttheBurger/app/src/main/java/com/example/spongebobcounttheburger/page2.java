45bf6b23c006ab268e7d5e8cc2798c48ab4ff4de
za18abf <73177727+za18abf@users.noreply.github.com>
2020-11-14 23:04:44 +0000

    Add some changes to the page 2 and page 3 activity
    I have add some images to the background for page 2 and page 3

--- SpongeBobCounttheBurger/app/src/main/java/com/example/spongebobcounttheburger/page2.java
+++ SpongeBobCounttheBurger/app/src/main/java/com/example/spongebobcounttheburger/page2.java
@@ -3,12 +3,34 @@ package com.example.spongebobcounttheburger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class page2 extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+        button = (Button)findViewById(R.id.pagebutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openpage3();
+
+            }
+        });
+
     }
+
+    public void openpage3 () {
+        Intent intent = new Intent(page2.this, page3.class);
+        startActivity(intent);
+
+
+    }
+
 }
